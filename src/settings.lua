c95d9a6ddb3d1418c4a48c8592672e4bbf307284
raiguard <caleb.heuer@gmail.com>
2020-03-18 23:42:59 -0600

    Remove mod GUI button setting

--- src/settings.lua
+++ src/settings.lua
@@ -1,24 +1,17 @@
 data:extend{
   {
     type = 'bool-setting',
-    name = 'rb-show-mod-gui-button',
-    setting_type = 'runtime-per-user',
-    default_value = false,
-    order = 'a'
-  },
-  {
-    type = 'bool-setting',
     name = 'rb-open-fluid-hotkey',
     setting_type = 'runtime-per-user',
     default_value = true,
-    order = 'b'
+    order = 'a'
   },
   {
     type = 'bool-setting',
     name = 'rb-show-hidden-objects',
     setting_type = 'runtime-per-user',
     default_value = false,
-    order = 'c'
+    order = 'b'
   },
   {
     type = 'string-setting',
@@ -26,6 +19,6 @@ data:extend{
     setting_type = 'runtime-per-user',
     default_value = 'material',
     allowed_values = {'crafter', 'material', 'recipe'},
-    order = 'd'
+    order = 'c'
   }
 }
