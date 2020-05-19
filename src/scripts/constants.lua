32788b7f0b0670c99d3a5e04c5b59ae4f70da688
raiguard <caleb.heuer@gmail.com>
2020-05-18 21:08:00 -0600

    Reformat constants file

--- src/scripts/constants.lua
+++ src/scripts/constants.lua
@@ -1,60 +1,64 @@
-local event = require("__flib__.event")
+local constants = {}
 
-local category_by_index = {"material", "recipe"}
+local event = require("__flib__.event")
 
 -- TODO dynamically generate translation tables from list of categories
 
-return {
-  blacklisted_recipe_categories = {
-    -- transport drones
-    ["fluid-depot"] = true,
-    ["transport-drone-request"] = true,
-    ["transport-fluid-request"] = true
-  },
-  category_by_index = category_by_index,
-  category_to_index = {material=1, recipe=2},
-  empty_lookup_tables = {
-    crafter = {lookup={}, sorted_translations={}},
-    material = {lookup={}, sorted_translations={}},
-    other = {lookup={}, sorted_translations={}},
-    recipe = {lookup={}, sorted_translations={}},
-    resource = {lookup={}, sorted_translations={}},
-    technology = {lookup={}, sorted_translations={}}
-  },
-  empty_translation_tables = {
-    crafter = {},
-    material = {},
-    other = {},
-    recipe = {},
-    resource = {},
-    technology = {}
-  },
-  info_guis = {material=true, recipe=true},
-  input_sanitizers = {
-    ["%("] = "%%(",
-    ["%)"] = "%%)",
-    ["%.^[%*]"] = "%%.",
-    ["%+"] = "%%+",
-    ["%-"] = "%%-",
-    ["^[%.]%*"] = "%%*",
-    ["%?"] = "%%?",
-    ["%["] = "%%[",
-    ["%]"] = "%%]",
-    ["%^"] = "%%^",
-    ["%$"] = "%%$"
-  },
-  interface_version = 2,
-  num_categories = #category_by_index,
-  open_fluid_types = {
-    ["fluid-wagon"] = true,
-    ["infinity-pipe"] = true,
-    ["offshore-pump"] = true,
-    ["pipe-to-ground"] = true,
-    ["pipe"] = true,
-    ["pump"] = true,
-    ["storage-tank"] = true
-  },
-  open_gui_event = event.generate_id(),
-  reopen_source_event = event.generate_id(),
-  unavailable_font_color = "255, 142, 142"
+constants.blacklisted_recipe_categories = {
+  -- transport drones
+  ["fluid-depot"] = true,
+  ["transport-drone-request"] = true,
+  ["transport-fluid-request"] = true
 }
+
+constants.content_panes = {home=true, material=true, recipe=true}
+
+constants.empty_lookup_tables = {
+  crafter = {lookup={}, sorted_translations={}},
+  material = {lookup={}, sorted_translations={}},
+  other = {lookup={}, sorted_translations={}},
+  recipe = {lookup={}, sorted_translations={}},
+  resource = {lookup={}, sorted_translations={}},
+  technology = {lookup={}, sorted_translations={}}
+}
+constants.empty_translation_tables = {
+  crafter = {},
+  material = {},
+  other = {},
+  recipe = {},
+  resource = {},
+  technology = {}
+}
+
+constants.search_input_sanitizers = {
+  ["%("] = "%%(",
+  ["%)"] = "%%)",
+  ["%.^[%*]"] = "%%.",
+  ["%+"] = "%%+",
+  ["%-"] = "%%-",
+  ["^[%.]%*"] = "%%*",
+  ["%?"] = "%%?",
+  ["%["] = "%%[",
+  ["%]"] = "%%]",
+  ["%^"] = "%%^",
+  ["%$"] = "%%$"
+}
+
+constants.interface_version = 2
+
+constants.open_fluid_types = {
+  ["fluid-wagon"] = true,
+  ["infinity-pipe"] = true,
+  ["offshore-pump"] = true,
+  ["pipe-to-ground"] = true,
+  ["pipe"] = true,
+  ["pump"] = true,
+  ["storage-tank"] = true
+}
+
+constants.open_gui_event = event.generate_id()
+constants.reopen_source_event = event.generate_id()
+
+constants.unavailable_font_color = "255, 142, 142"
+
+return constants
