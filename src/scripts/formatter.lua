626b836f937a698f17eccff162ddc1f9b2b295ec
raiguard <caleb.heuer@gmail.com>
2020-07-19 12:41:08 -0600

    Show info label even when it should be hidden

--- src/scripts/formatter.lua
+++ src/scripts/formatter.lua
@@ -41,18 +41,20 @@ local function get_should_show(obj_data, player_data)
   return false, is_hidden, is_researched
 end
 
-local function caption_formatter(obj_data, player_data, is_hidden, amount)
+local function caption_formatter(obj_data, player_data, is_hidden, amount, is_info_label)
   local player_settings = player_data.settings
   local translations = player_data.translations
   local translation_key = obj_data.internal_class == "material" and obj_data.sprite_class.."."..obj_data.prototype_name or obj_data.prototype_name
   local translation = translations[obj_data.internal_class][translation_key]
+  local font_prefix = is_info_label and "" or "[font=default-semibold]"
+  local font_suffix = is_info_label and "" or "[/font]"
   local glyph = ""
   if player_settings.show_glyphs then
     local glyph_char = constants.class_to_font_glyph[obj_data.internal_class] or constants.class_to_font_glyph[obj_data.sprite_class]
     glyph = "[font=RecipeBook]"..glyph_char.."[/font]  "
   end
-  local hidden_string = is_hidden and "[font=default-semibold]("..translations.gui.hidden_abbrev..")[/font]  " or ""
-  local amount_string = amount and "[font=default-semibold]"..amount.."[/font]  " or ""
+  local hidden_string = is_hidden and font_prefix.."("..translations.gui.hidden_abbrev..")"..font_suffix.."  " or ""
+  local amount_string = amount and font_prefix..amount..font_suffix.."  " or ""
   local name = player_settings.show_internal_names and obj_data.prototype_name or translation
   return
     glyph
@@ -114,15 +116,15 @@ local tooltip_formatters = {
   }
 }
 
-local function format_item(obj_data, player_data, amount_string)
+local function format_item(obj_data, player_data, amount_string, is_info_label)
   local should_show, is_hidden, is_researched = get_should_show(obj_data, player_data)
-  if should_show then
+  if is_info_label or should_show then
     -- format and return
     local formatter_subtable = tooltip_formatters[obj_data.internal_class]
     return
       true,
       is_researched and "rb_list_box_item" or "rb_unresearched_list_box_item",
-      caption_formatter(obj_data, player_data, is_hidden, amount_string),
+      caption_formatter(obj_data, player_data, is_hidden, amount_string, is_info_label),
       formatter_subtable.tooltip(obj_data, player_data, is_hidden, is_researched),
       formatter_subtable.enabled
   else
@@ -130,16 +132,20 @@ local function format_item(obj_data, player_data, amount_string)
   end
 end
 
-function formatter.format(obj_data, player_data, amount_string)
+function formatter.format(obj_data, player_data, amount_string, is_info_label)
   local player_index = player_data.player_index
   local cache = caches[player_index]
   local _, is_researched = get_properties(obj_data, player_data.force_index)
-  local cache_key = obj_data.sprite_class.."."..obj_data.prototype_name.."."..(amount_string or "false").."."..tostring(is_researched)
+  local cache_key = obj_data.sprite_class
+  .."."..obj_data.prototype_name
+  .."."..(amount_string or "false")
+  .."."..tostring(is_researched)
+  .."."..tostring(is_info_label)
   local cached_return = cache[cache_key]
   if cached_return then
     return table.unpack(cached_return)
   else
-    local should_show, style, caption, tooltip, enabled = format_item(obj_data, player_data, amount_string)
+    local should_show, style, caption, tooltip, enabled = format_item(obj_data, player_data, amount_string, is_info_label)
     cache[cache_key] = { should_show, style, caption, tooltip, enabled }
     return should_show, style, caption, tooltip, enabled
   end
