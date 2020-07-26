eb5cceb1761d5c55fd58878055dd3f33929689ea
raiguard <caleb.heuer@gmail.com>
2020-07-26 13:24:01 -0600

    Use function instead of reading global directly

--- src/scripts/on-tick.lua
+++ src/scripts/on-tick.lua
@@ -4,19 +4,15 @@ local event = require("__flib__.event")
 local translation = require("__flib__.translation")
 
 function on_tick.handler(e)
-  local deregister = true
-  if global.__flib.translation.translating_players_count > 0 then
-    deregister = false
+  if translation.translating_players_count() > 0 then
     translation.iterate_batch(e)
-  end
-
-  if deregister then
+  else
     event.on_tick(nil)
   end
 end
 
 function on_tick.update()
-  if global.__flib and global.__flib.translation.translating_players_count > 0 then
+  if global.__flib and translation.translating_players_count() > 0 then
     event.on_tick(on_tick.handler)
   end
 end
