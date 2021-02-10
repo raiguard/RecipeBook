851840741f47413b9f0979c59f83d79d29bb444a
raiguard <caleb.heuer@gmail.com>
2021-02-09 21:48:16 -0700

    Update settings titlebar flow

--- src/scripts/gui/main/base.lua
+++ src/scripts/gui/main/base.lua
@@ -202,7 +202,7 @@ function main_gui.build(player, player_table)
         visible = false,
         ref = {"settings", "window"},
         children = {
-          {type = "flow", ref = {"settings", "titlebar_flow"}, children = {
+          {type = "flow", style = "flib_titlebar_flow", ref = {"settings", "titlebar_flow"}, children = {
             {type = "label", style = "frame_title", caption = {"gui-menu.settings"}, ignored_by_interaction = true},
             {type = "empty-widget", style = "flib_dialog_titlebar_drag_handle", ignored_by_interaction = true},
           }},
