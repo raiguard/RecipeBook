99d323865047f35a754db6df17b240fab4ecfd02
raiguard <caleb.heuer@gmail.com>
2021-02-11 18:28:33 -0700

    Add placeable by to the crafter page

--- src/scripts/gui/main/pages/crafter.lua
+++ src/scripts/gui/main/pages/crafter.lua
@@ -5,7 +5,8 @@ local crafter_page = {}
 function crafter_page.build()
   local elems =  {
     info_list_box.build({"rb-gui.compatible-recipes"}, 1, {"crafter", "recipes"}),
-    info_list_box.build({"rb-gui.unlocked-by"}, 1, {"crafter", "unlocked_by"})
+    info_list_box.build({"rb-gui.unlocked-by"}, 1, {"crafter", "unlocked_by"}),
+    info_list_box.build({"rb-gui.placeable-by"}, 1, {"crafter", "placeable_by"})
   }
 
   return elems
@@ -21,11 +22,8 @@ function crafter_page.update(int_name, gui_data, player_data)
     player_data,
     {max_listbox_height = obj_data.researched_forces and 11 or 15}
   )
-  info_list_box.update(
-    obj_data.unlocked_by,
-    refs.crafter.unlocked_by,
-    player_data
-  )
+  info_list_box.update(obj_data.unlocked_by, refs.crafter.unlocked_by, player_data)
+  info_list_box.update(obj_data.placeable_by, refs.crafter.placeable_by, player_data)
 end
 
 return crafter_page
