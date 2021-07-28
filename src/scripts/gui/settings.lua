ede68ddc1049b1399d8ee0ddb197b52264d0c3c9
raiguard <caleb.heuer@gmail.com>
2021-07-27 21:01:54 -0600

    Add toolbar and search button mockup

--- src/scripts/gui/settings.lua
+++ src/scripts/gui/settings.lua
@@ -15,11 +15,35 @@ function settings_gui.build(player, player_table)
       actions = {
         on_closed = {gui = "settings", action = "close"},
       },
-      {type = "frame", style = "inside_deep_frame_for_tabs",
-        {type = "tabbed-pane", style = "tabbed_pane_with_no_side_padding",
-          {tab = {type = "tab", caption = {"gui.rb-general"}}, content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}},
-          {tab = {type = "tab", caption = {"gui.rb-categories"}}, content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}},
-          {tab = {type = "tab", caption = {"gui.rb-pages"}}, content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}},
+      -- {type = "flow", style = "flib_titlebar_flow", ref = {"titlebar", "flow"},
+      --   {type = "label", style = "frame_title", caption = {"gui.rb-settings"}, ignored_by_interaction = true},
+      --   {type = "empty-widget", style = "flib_titlebar_drag_handle", ignored_by_interaction = true},
+      --   util.frame_action_button(
+      --     "utility/search",
+      --     {"gui.rb-search-instruction"},
+      --     {"titlebar", "search_button"},
+      --     {gui = "settings", action = "toggle_search"}
+      --   )
+      -- },
+      {type = "frame", style = "inside_deep_frame", direction = "vertical",
+        {type = "frame", style = "subheader_frame",
+          {type = "empty-widget", style = "flib_horizontal_pusher"},
+          {type = "sprite-button", style = "tool_button", sprite = "utility/search_icon", tooltip = {"gui.rb-search-instruction"}},
+          {type = "sprite-button", style = "tool_button_red", sprite = "utility/reset", tooltip = {"reset-to-defaults-disabled"}},
+        },
+        {type = "tabbed-pane", style = "tabbed_pane_with_no_side_padding", style_mods = {top_padding = 12},
+          {
+            tab = {type = "tab", caption = {"gui.rb-general"}},
+            content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}
+          },
+          {
+            tab = {type = "tab", caption = {"gui.rb-categories"}},
+            content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}
+          },
+          {
+            tab = {type = "tab", caption = {"gui.rb-pages"}},
+            content = {type = "scroll-pane", style = "flib_naked_scroll_pane_under_tabs", style_mods = {width = 500, height = 500}}
+          },
         }
       },
       {type = "flow", style = "dialog_buttons_horizontal_flow",
