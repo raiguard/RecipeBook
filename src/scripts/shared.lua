b40df17943e9b7c984cb79e6a773abf7ee739ff9
raiguard <caleb.heuer@gmail.com>
2021-07-31 16:15:04 -0600

    Micro

--- src/scripts/shared.lua
+++ src/scripts/shared.lua
@@ -1,2 +1,3 @@
 -- this table is populated from individual files to solve dependency loops
+-- TODO: Turn GUI tables into metatables so you can call methods on them directly, removing the need for `shared`!
 return {}
