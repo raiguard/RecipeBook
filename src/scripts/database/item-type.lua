ac9376c439c173f93eb5cf3fea07bf533adb86fb
Caleb Heuer <ch@raiguard.me>
2022-06-12 21:30:38 -0600

    Remove mining tool exclusions
    It caused more problems than it solved. Some areas were ignoring the
    item, and others weren't, so the data set was inconsistent, causing
    crashes.
    
    Fixes: https://todo.sr.ht/~raiguard/factorio-mods/29

--- src/scripts/database/item-type.lua
+++ src/scripts/database/item-type.lua
@@ -1,20 +1,18 @@
 return function(database, dictionaries)
   for name, prototype in pairs(global.prototypes.item) do
-    if prototype.type ~= "mining-tool" then
-      local type = prototype.type
-      local type_data = database.item_type[type]
-      if not type_data then
-        type_data = {
-          class = "item_type",
-          items = {},
-          prototype_name = type,
-        }
-        database.item_type[type] = type_data
-        dictionaries.item_type:add(type, { "item-type." .. type })
-        dictionaries.item_type_description:add(type, { "item-type-description." .. type })
-      end
-
-      table.insert(type_data.items, { class = "item", name = name })
+    local type = prototype.type
+    local type_data = database.item_type[type]
+    if not type_data then
+      type_data = {
+        class = "item_type",
+        items = {},
+        prototype_name = type,
+      }
+      database.item_type[type] = type_data
+      dictionaries.item_type:add(type, { "item-type." .. type })
+      dictionaries.item_type_description:add(type, { "item-type-description." .. type })
     end
+
+    table.insert(type_data.items, { class = "item", name = name })
   end
 end
