e61766925348f35df826e812306d021153e844ee
Caleb Heuer <ch@raiguard.me>
2023-01-22 13:26:52 -0700

    Fix migrations

--- control.lua
+++ control.lua
@@ -1,8 +1,9 @@
 local handler = require("__core__/lualib/event_handler")
 
+handler.add_lib(require("__RecipeBook__/migrations"))
+
 handler.add_lib(require("__flib__/dictionary-lite"))
 handler.add_lib(require("__flib__/gui-lite"))
 
 handler.add_lib(require("__RecipeBook__/database"))
 handler.add_lib(require("__RecipeBook__/gui"))
-handler.add_lib(require("__RecipeBook__/migrations"))
--- migrations.lua
+++ migrations.lua
@@ -1,10 +1,17 @@
+local dictionary = require("__flib__/dictionary-lite")
 local migration = require("__flib__/migration")
+
+local database = require("__RecipeBook__/database")
+local gui = require("__RecipeBook__/gui")
 
 local by_version = {
 	["4.0.0"] = function()
 		-- NUKE EVERYTHING
 		global = {}
-		-- TODO: Re-init
+		-- Re-init
+		dictionary.on_init()
+		database.on_init()
+		gui.on_init()
 	end,
 }
 
