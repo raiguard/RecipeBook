5f518cb7aa0c4cc90a687ec7219c54b7c73b99d7
Caleb Heuer <ch@raiguard.me>
2025-03-03 13:34:09 -0700

    Mark all unbarrelling recipes as not unlocking their results

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,2 +1,8 @@
 require("prototypes.pack-overrides")
 require("prototypes.pack-sprites")
+
+for recipe_name, recipe in pairs(data.raw.recipe) do
+  if string.find(recipe_name, "empty%-.*%-barrel") then
+    recipe.unlock_results = false
+  end
+end
