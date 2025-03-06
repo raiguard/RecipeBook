923206830d9783b28db44c1478a0d5f3ed4a173e
Caleb Heuer <ch@raiguard.me>
2025-03-05 20:06:36 -0700

    Unbarreling recipes were fixed in 2.0.39

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,1 @@
 require("prototypes.pack-sprites")
-
-for recipe_name, recipe in pairs(data.raw.recipe) do
-  if string.find(recipe_name, "empty%-.*%-barrel") then
-    recipe.unlock_results = false
-  end
-end
