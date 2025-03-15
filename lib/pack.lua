c600af5c65aa038b82f4ef3f84e7d392f8223b9e
Caleb Heuer <ch@raiguard.me>
2025-03-14 23:27:18 -0600

    Update licensing

--- lib/pack.lua
+++ lib/pack.lua
@@ -1,5 +1,18 @@
 -- Based on "Big Data String Library" by dodo.the.last.
 -- https://mods.factorio.com/mod/big-data-string
+--
+-- This is free and unencumbered software released into the public domain.
+--
+-- Anyone is free to copy, modify, publish, use, compile, sell, or
+-- distribute this software, either in source code form or as a compiled
+-- binary, for any purpose, commercial or non-commercial, and by any
+-- means.
+--
+-- In jurisdictions that recognize copyright laws, the author or authors
+-- of this software dedicate any and all copyright interest in the
+-- software to the public domain. We make this dedication for the benefit
+-- of the public at large and to the detriment of our heirs and
+-- successors.
 
 local MAX = 10 * 20 ^ 21 -- 20 parameters * 20 recursion depth
 
